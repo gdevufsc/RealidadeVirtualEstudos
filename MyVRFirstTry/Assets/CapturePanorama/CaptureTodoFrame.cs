cc61af24837b1c909736c0f7dcd23348d7d3174e
kkaldas <cainacc@hotmail.com>
2018-04-28 03:35:39 -0300

    Tentando gravar video360 na cena de nome sugestivo

---
+++ MyVRFirstTry/Assets/CapturePanorama/CaptureTodoFrame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using CapturePanorama;
+
+public class CaptureTodoFrame : MonoBehaviour {
+
+    public CapturePanorama.CapturePanorama capturePanorama;
+    public string baseFileName;
+    int num = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.R))
+        {
+            num++;
+            capturePanorama.CaptureScreenshotAsync( baseFileName + num );
+        }
+	}
+}
