62ab601173ecbf7d4a80c665bd2296110a52b7ff
kkaldas <cainacc@hotmail.com>
2018-05-14 13:04:05 -0300

    adicionada cena prototipo001
    está perguntando se ja viu o bloco
    Tem um investigador

---
+++ MyVRFirstTry/Assets/AssetsNovosOrganizados/Scripts/Investigador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Investigador : MonoBehaviour {
+
+    //eu poderia chamar de boca, mas no modelo dele tem os olhos que eh bem perto
+    public AudioSource olhosDoInvestigador; //coloquei a audiosource nos olhos
+    public AudioClip audioVcJahViu;
+    public float tempoAtehFalar = 3; //em segundos
+    public GameObject HeadSeN;
+
+	void Start () {
+        Invoke("VcJahViu" , tempoAtehFalar);
+        HeadSeN.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void VcJahViu () {
+        olhosDoInvestigador.PlayOneShot(audioVcJahViu);
+    }
+}
